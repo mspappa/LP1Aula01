f7a8a87d8826c219842f83881f6f5996fee8009a
mspappa <marco.duarte.santos@hotmail.com>
2022-02-25 23:30:36 +0000

    Varias Strings update 2

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -10,11 +10,23 @@ namespace OlaMundo
         static void Main(string[] args)
         {
             string a = "Um tab\t e um copyright\u03A9";
-            string b = @"Nao e’ um codigo Unicode \u1234, nao e’ nova linha \n";
-            string c = "\"Esta string esta ’ entre aspas\"";
-            string d = @"""Esta string esta ’ entre aspas""";
+            string b = @"Nao é um codigo Unicode \u1234, nao é nova linha \n";
+            string c = "\"Esta string está entre aspas\"";
+            string d = @"""Esta string está entre aspas""";
+
+            string x = "a" + 2;
+            string y = "abc" + x;
+
+            string z = $"valor de x é {x}";
+            string w = $"{x} mais {y} e’ igual a {x + y}";
+
+            String.Format("Y e’ {1}, X e’ {0}", x, y);
+            
 
             Console.WriteLine(a + "\n" + b + "\n" + c + "\n" + d);
+            Console.WriteLine("\n" + x + "\n" + y);
+            Console.WriteLine("\n" + z + "\n" + w);
+            Console.WriteLine(@"Verbatim com x = {0}", x);
         }
     }
 }
