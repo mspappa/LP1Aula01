08c0852abd31bae1b52bad1c8ffb0aa1d840bd50
mspappa <marco.duarte.santos@hotmail.com>
2022-02-25 20:44:37 +0000

    Add bye world!

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Bye world");
+using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Bye world!");
+        }
+    }
+}
+
