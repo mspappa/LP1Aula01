acf3a3ee11d6b83bd4c634a7a79f26cc618fe453
mspappa <marco.duarte.santos@hotmail.com>
2022-02-25 20:50:53 +0000

    Second commit

---
+++ Argumentos/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace OlaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args);
+            }
+        }
+    }
+}
