db0cf24f4b88c0aecbaab503d6d6e06bb47e1b6f
mspappa <marco.duarte.santos@hotmail.com>
2022-02-25 22:38:05 +0000

    Fourth commit

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace OlaMundo
+///
+{
+    class Program
+    ///
+    {
+        static void Main(string[] args)
+        {
+            int i = 0x10;
+            uint j = 0x10U;
+            int x = 0b1011_0110;
+            int n = 0b10110110;
+            double y = 1_712_432.23;
+        }
+    }
+}
